0abf5dacee5daa909e0598315897ad6a55fa9c17
hamzafoy <hamzaoflouisville@gmail.com>
2022-09-06 13:03:13 -0400

    created a function for a script to run the storyBook printer.

--- storybookReader.py
+++ storybookReader.py
@@ -1,11 +1,15 @@
 # This imported module facilitates the opening of URLs to derive data
 from urllib.request import urlopen
 
-story = urlopen('http://sixty-north.com/c/t.txt')
-story_words = []
-for line in story:
-    #The following line appends each decoded byte, replaces the \n character, and capitalizes each new line
-    story_words.append(line.decode('utf8').replace('\n', '. ').capitalize())
-wholeStory = ''.join(story_words)
-story.close()
-print(wholeStory)
+def fetch_words():
+    story = urlopen('http://sixty-north.com/c/t.txt')
+    story_words = []
+    for line in story:
+        #The following line appends each decoded byte, replaces the \n character, and capitalizes each new line
+        story_words.append(line.decode('utf8').replace('\n', '. ').capitalize())
+    wholeStory = ''.join(story_words)
+    story.close()
+    print(wholeStory)
+
+if __name__ == '__main__':
+    fetch_words()
