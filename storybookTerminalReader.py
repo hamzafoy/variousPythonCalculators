2f294141e20bdf2e06c87d338b5063dea6ba3cea
hamzafoy <hamzaoflouisville@gmail.com>
2022-09-06 15:05:22 -0400

    adding a new script that takes a url containing .txt file as command line parameter and prints story to terminal.

---
+++ storybookTerminalReader.py
@@ -0,0 +1,23 @@
+# This imported module facilitates the opening of URLs to derive data
+from urllib.request import urlopen
+# Allows access to command line parameters passed in terminal
+import sys
+
+def fetch_words(url):
+    story = urlopen(url)
+    story_words = []
+    for line in story:
+        #The following line appends each decoded byte, replaces the \n character, and capitalizes each new line
+        story_words.append(line.decode('utf8').replace('\n', '. ').capitalize())
+    wholeStory = ''.join(story_words)
+    story.close()
+    return wholeStory
+
+def main(url):
+    storybook = fetch_words(url)
+    print(storybook)
+
+# Enables taking URLs of .txt files and will print them.
+# Use http://textfiles.com/stories/snowmaid.txt as an example
+if __name__ == '__main__':
+    main(sys.argv[1])
