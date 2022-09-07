71b643447120fbac8c177bcce46b1cc4b5228964
hamzafoy <hamzaoflouisville@gmail.com>
2022-09-07 18:54:07 -0400

    developed the equation to render the exchanged amount of currency with 2 args.

--- currencyExchangeCalculator.py
+++ currencyExchangeCalculator.py
@@ -5,9 +5,23 @@ import json
 from dotenv import load_dotenv
 load_dotenv()
 
+# currencies = {
+#     'England': 'GBP',
+#     'Europe': 'EUR',
+#     'Iraq': 'IQD',
+#     'Japan': 'JPY',
+#     'United Arab Emirates': 'AED'
+# }
+
 apiKey = os.environ.get('CURRENCY_API_KEY')
 url = "https://openexchangerates.org/api/latest.json?app_id="+str(apiKey)
-
 request = requests.get(url)
 json = request.json()
 print(json['rates'])
+
+def exchange_currency(amount, currencyabbrev):
+    print(json['rates'][currencyabbrev])
+    expectedExchange = (amount * json['rates'][currencyabbrev])
+    return expectedExchange
+
+print(exchange_currency(10, 'JPY'))
