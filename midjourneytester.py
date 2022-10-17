0f4989ec53fe7c8841424a117227f032a0b471dc
hamzafoy <hamzaoflouisville@gmail.com>
2022-10-17 17:42:30 -0400

    playing around with Midjourney user input data.

---
+++ midjourneytester.py
@@ -0,0 +1,7 @@
+import json
+
+with open('general-01_2022_06_20.json') as midjourneydata:
+    midjourneydataset = json.load(midjourneydata)
+
+print(len(midjourneydataset))
+print(midjourneydataset["messages"][0])
