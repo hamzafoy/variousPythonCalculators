161b70f7eb209be315104ff020a2c11e1f7b6ce6
hamzafoy <hamzaoflouisville@gmail.com>
2022-10-25 17:55:41 -0400

    cleaning up code.

--- workingwithdatetime.py
+++ workingwithdatetime.py
@@ -13,7 +13,7 @@ print("The first day of the year is {:%A %d %B %Y}".format(start_date))
 print("{date:%A} {date.day} {date:%B} {date.year}".format(date=current_date))
 
 lapse_in_days = current_date - start_date
-print(lapse_in_days.days)
+print("The number of days between the start of the year and now is: " + str(lapse_in_days.days))
 
 #Playing with time
 current_time = time.localtime()
@@ -28,5 +28,4 @@ centraltime = datetime.timezone(datetime.timedelta(hours=-5), "CST")
 departure = datetime.datetime(year=2023, month=6, day=1, hour=9, minute=20, tzinfo=centraltime)
 arrival = datetime.datetime(year=2023, month=6, day=2, hour=7, minute=55, tzinfo=gulftime)
 timelapsed = arrival - departure
-print(timelapsed.seconds)
 print("The flight from Chicago to Abu Dhabi will take " + str(round(timelapsed.seconds / 60 / 60, 2)) + " hours.")
