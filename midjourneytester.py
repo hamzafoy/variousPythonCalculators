3a1d7a8c5925fbaa235207402c4576c0043c1e03
hamzafoy <hamzaoflouisville@gmail.com>
2022-10-25 16:57:28 -0400

    starting a file to play around with the datetime module in Python.

--- midjourneytester.py
+++ midjourneytester.py
@@ -4,4 +4,4 @@ with open('general-01_2022_06_20.json') as midjourneydata:
     midjourneydataset = json.load(midjourneydata)
 
 print(len(midjourneydataset))
-print(midjourneydataset["messages"][0])
+print(midjourneydataset["messages"][0][0])
---
+++ workingwithdatetime.py
@@ -0,0 +1,6 @@
+import datetime
+start_date = datetime.date(year=2022, month=1, day=1)
+current_date = datetime.date.today()
+
+print(start_date)
+print(current_date)
